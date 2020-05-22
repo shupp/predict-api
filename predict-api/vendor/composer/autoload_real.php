b29a80d4556494b6309ffb72b3136900ed1821ab
Bill Shupp <hostmaster@shupp.org>
2020-05-21 22:05:25 -0700

    Set up include_path for old Predict library

--- predict-api/vendor/composer/autoload_real.php
+++ predict-api/vendor/composer/autoload_real.php
@@ -26,6 +26,10 @@ class ComposerAutoloaderInit66ff2d260f205ee0dd4882cb6f1f51d6
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit66ff2d260f205ee0dd4882cb6f1f51d6', 'loadClassLoader'));
 
+        $includePaths = require __DIR__ . '/include_paths.php';
+        $includePaths[] = get_include_path();
+        set_include_path(implode(PATH_SEPARATOR, $includePaths));
+
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
